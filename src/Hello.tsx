2cd5e911d1ea9102c6605432dd451244d96f4da8
Masashi Salvador Mitsuzawa <masashisalvador57f@gmail.com>
2017-05-15 23:48:21 +0900

    remove debug code

--- src/Hello.tsx
+++ src/Hello.tsx
@@ -1,10 +1,6 @@
 import * as React from 'react';
 import * as firebase from 'firebase';
 
-console.log("====");
-console.log(firebase);
-console.log("===----");
-
 export interface Props {
   content: string;
 }
